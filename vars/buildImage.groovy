f96538257b7d9fad981bc47f2f4098a7a258040d
Olawale Olanrewaju <olanrewajume@yahoo.com>
2024-06-18 12:56:41 +0100

    Fixed dockerhub login again

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 
 def call(String imageName){
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-login', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh "docker build -t $imageName ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh "docker push $imageName"
